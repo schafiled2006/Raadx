2448995556d76e41bd5f4b4fd164d0a4d42505d4
mikaeil <mikyyangryy@gmail.com>
2026-06-23 00:32:36 +0330

    Add admin panel for adding music without coding & add and fix supabase connection

--- Views/js/index.js
+++ Views/js/index.js
@@ -18,8 +18,67 @@ const messageUpdate = () => {
     }
 
     messageElemnt.textContent = text;
-
 };
 
+document.addEventListener("DOMContentLoaded", () => {
+    messageUpdate();
+    loadAdminSongs();
+});
 
-document.addEventListener("DOMContentLoaded", messageUpdate);
+
+const SUPABASE_URL = "https://jxyyvqkmyeythspvnwid.supabase.co";
+const SUPABASE_KEY = "sb_publishable_Qlw9rzbRm6VhDHmIk3SzUA_uxSpO6sf";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function loadAdminSongs() {
+
+    const container = document.getElementById("admin-songs-list");
+
+    const { data, error } = await supabaseClient
+        .from("songs")
+        .select("*")
+        .order("created_at", { ascending: false })
+        .limit(10);
+
+    if (error) {
+        container.innerHTML = "<p>Failed to load songs</p>";
+        console.error(error);
+        return;
+    }
+
+    container.innerHTML = "";
+
+    data.forEach(song => {
+
+        container.innerHTML += `
+            <div class="admin-song-card">
+
+                <div class="admin-song-left">
+
+                    <img
+                        class="admin-song-cover"
+                        src="${song.cover_url}"
+                        alt="${song.title}"
+                    >
+
+                    <div class="admin-song-info">
+                        <h3>${song.title}</h3>
+                        <p>${song.artist}</p>
+                    </div>
+
+                </div>
+
+                <div class="admin-song-btn">
+                    <audio controls>
+                        <source src="${song.audio_url}">
+                    </audio>
+                </div>
+
+            </div>
+        `;
+    });
+}
